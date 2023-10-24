44cf156b672c934327a6ebd46bb2c101df16c553
pyxel <83606447+pyxel@users.noreply.github.com>
2023-10-24 17:07:32 +0000

    Filters

--- pages/Drivers.py
+++ pages/Drivers.py
@@ -7,10 +7,24 @@ def show_table():
 
     sql = "select max(round) as round from round_player_standing"
 
-    st.header(f"Round {duckdb.sql(sql).df().loc[0]['round']}")
+    latest_round = duckdb.sql(sql).df().loc[0]['round']
 
-    sql = """
+    st.header(f"Driver results")
+
+    sql = "select driver from driver order by driver"
+
+    selected_drivers = st.multiselect("Select drivers:", default = "PER", options = duckdb.sql(sql).df())
+
+    sql = "select distinct round from result order by round desc"
+
+    selected_rounds = st.multiselect("Select rounds:", default = latest_round, options = duckdb.sql(sql).df())
+
+    driver_filter = f"""and s.driver in ({','.join([f"'{d}'" for d in selected_drivers])})""" if len(selected_drivers) > 0 else ""
+    round_filter = f"""and s.round in ({','.join([f"'{r}'" for r in selected_rounds])})""" if len(selected_rounds) > 0 else ""
+
+    sql = f"""
         select
+            s.round,
             s.driver,
             s.points_this_round,
             s.points,
@@ -19,11 +33,14 @@ def show_table():
         from standing s
         join multiplier m on m.driver = s.driver
             and m.round = s.round
-        where s.round = (select max(round) from round_player_standing)
-        order by 3 desc, 2, 1
+        --where s.round = (select max(round) from round_player_standing)
+        where 1=1
+            {driver_filter}
+            {round_filter}
+        order by s.round, s.driver
     """
     
-    st.dataframe(duckdb.sql(sql).df(), height = 800)
+    st.dataframe(duckdb.sql(sql).df())
 
 
 
--- pages/Players.py
+++ pages/Players.py
@@ -7,19 +7,39 @@ def show_table():
 
     sql = "select max(round) as round from round_player_standing"
 
-    st.header(f"Player stakes - round {duckdb.sql(sql).df().loc[0]['round']}")
+    latest_round = duckdb.sql(sql).df().loc[0]['round']
 
-    sql = """
+    st.header(f"Player stakes")
+
+    sql = "select distinct player from stake order by player"
+
+    selected_players = st.multiselect("Select players:", options = duckdb.sql(sql).df())
+
+    sql = "select distinct round from result order by round desc"
+
+    selected_rounds = st.multiselect("Select rounds:", default = latest_round, options = duckdb.sql(sql).df())
+
+    player_filter = f"""and s.player in ({','.join([f"'{d}'" for d in selected_players])})""" if len(selected_players) > 0 else ""
+    round_filter = ""
+    if len(selected_rounds) > 0:
+        round_filter = 'and (' + ' or '.join([f"{r} between start_round and end_round" for r in selected_rounds]) + ')'
+
+    sql = f"""
         select
+            s.start_round,
+            s.end_round,
             s.player,
             s.driver,
             s.stake
         from stake s
-        where end_round = 22
+        where 1=1
+            {player_filter}
+            {round_filter}
+        order by s.start_round, s.end_round, s.player, s.stake
         
     """
     
-    st.dataframe(duckdb.sql(sql).df(), height = 800)
+    st.dataframe(duckdb.sql(sql).df())
 
 
 
